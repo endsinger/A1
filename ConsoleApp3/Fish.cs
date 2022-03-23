97b8137ad754b8c555344f4ec50a2260e447447b
endsinger <102259704+endsinger@users.noreply.github.com>
2022-03-24 01:07:27 +0400

    Add files via upload

---
+++ ConsoleApp3/Fish.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    [Serializable]
+    class Fish
+    {
+        private string name;
+        private int _long;
+        private int weight;
+        private string color;
+        //public Fish()
+        //{
+        //    Console.WriteLine("Имя " + name + "Длина " + _long + "Вес " + weight + "Цвет " + color);
+        //}
+
+        public void Characters(string _name, int __long, int _weight, string _color)
+        {
+            name = _name;
+            _long = __long;
+            weight = _weight;
+            color = _color;
+
+        }
+        public void Print()
+        {
+            Console.WriteLine("Имя " + name +"Длина "+ _long + "Вес " + weight +"Цвет " + color );
+        }
+    
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fish karas = new Fish();
+            karas.Characters("Карась", 100, 200, "Зеленый");
+            karas.Print();
+
+            Fish okun = new Fish();
+            okun.Characters("Окунь", 100, 200, "Зеленый");
+            okun.Print();
+            
+            Fish akula = new Fish();
+            akula.Characters("Акула", 100, 200, "Зеленый");
+            akula.Print();
+            
+            Fish dolphin = new Fish();
+            dolphin.Characters("Дерьфин", 100, 200, "Зеленый");
+            dolphin.Print();
+            
+            Fish seledka = new Fish();
+            seledka.Characters("Селедка", 100, 200, "Зеленый");
+            seledka.Print();
+            
+            Fish moiva = new Fish();
+            moiva.Characters("Мойва", 100, 200, "Зеленый");
+            moiva.Print();
+            
+            Fish karp = new Fish();
+            karp.Characters("Карп", 100, 200, "Зеленый");
+            karp.Print();
+            
+            Fish SeaStar = new Fish();
+            SeaStar.Characters("Морская звезда", 100, 200, "Зеленый");
+            SeaStar.Print();
+            
+            Fish som = new Fish();
+            som.Characters("Сом", 100, 200, "Зеленый");
+            som.Print();
+            
+            Fish bream = new Fish();
+            bream.Characters("Лещь", 100, 200, "Зеленый");
+            bream.Print();
+        }
+    }
+}
